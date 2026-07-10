c3d3fb503e34c9bc361ebf4e368a45545877d3ed
Rahul Bastia <151760382+rahulbastia00@users.noreply.github.com>
2026-07-10 18:59:02 +0530

    updated the gitignore

--- Scripts/producer_marketaux.py
+++ Scripts/producer_marketaux.py
@@ -4,19 +4,26 @@ from confluent_kafka import Producer
 from models import MarketAuxResponse
 
 load_dotenv()
-producer = Producer({'bootstrap.servers': os.getenv("KAFKA_BOOTSTRAP_SERVERS")})
+producer = Producer({"bootstrap.servers": os.getenv("KAFKA_BOOTSTRAP_SERVERS")})
+
 
 def poll():
-    url = f"https://api.marketaux.com/v1/news/all?symbols=GOOGL,MSFT,AMZN,NVDA&api_token={os.getenv('MARKETAUX_API_KEY')}"
+    url = f"https://api.marketaux.com/v1/news/all?symbols=GOOGL,MSFT,AMZN,NVDA&api_token={os.getenv('MARKETAUX')}"
     try:
         res = requests.get(url).json()
         validated = MarketAuxResponse(**res)
         for article in validated.data:
-            producer.produce(topic='raw_marketaux_news', key=article.uuid, value=article.model_dump_json())
+            producer.produce(
+                topic="raw_marketaux_news",
+                key=article.uuid,
+                value=article.model_dump_json(),
+            )
         producer.flush()
         print(f"[MarketAux] Ingested {len(validated.data)} articles.")
-    except Exception as e: print(f"Error: {e}")
+    except Exception as e:
+        print(f"Error: {e}")
+
 
 while True:
     poll()
-    time.sleep(300) # Wait 5 minutes
+    time.sleep(300)  # Wait 5 minutes
